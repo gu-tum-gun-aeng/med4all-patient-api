6bdeb5f1b9c611d44ffda40ca94bdc3d1f77565e
veerapat.b <veerapat.b@ava.fund>
2021-08-10 19:48:35 +0700

    added patient producer controller unit test

---
+++ src/tests/mock/patient.mock.test.ts
@@ -0,0 +1,7 @@
+import { CreatePatientDto } from '@dtos/patient.dto';
+
+const patientModel: CreatePatientDto[] = [
+  { id: 0, cdPersonFirstName: '', cdPersonLastName: '', cdPersonPhone1: '', emPatientCommitStatusCode: 1, crProvinceCode: '', crAmpurCode: '' },
+];
+
+export default patientModel;
