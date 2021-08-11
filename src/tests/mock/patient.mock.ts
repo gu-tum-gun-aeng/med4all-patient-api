c04d9d299cfc0df56e93c84ae88b71cd88ea60d3
veerapat.b <veerapat.b@ava.fund>
2021-08-11 11:54:13 +0700

    added more patient mock

--- src/tests/mock/patient.mock.ts
+++ src/tests/mock/patient.mock.ts
@@ -1,7 +1,55 @@
 import { CreatePatientDto } from '@dtos/patient.dto';
 
-const patientModel: CreatePatientDto[] = [
-  { id: 0, cdPersonFirstName: '', cdPersonLastName: '', cdPersonPhone1: '', emPatientCommitStatusCode: 1, crProvinceCode: '', crAmpurCode: '' },
-];
+export const validPatientRequestWithPersonIDMock: CreatePatientDto = {
+  id: 1,
+  cdPersonID: '0000000000000',
+  cdPersonFirstName: 'John',
+  cdPersonLastName: 'Doe',
+  cdPersonPhone1: '0811231234',
+  emPatientCommitStatusCode: 1,
+  crProvinceCode: '00',
+  crAmpurCode: '01',
+};
 
-export default patientModel;
+export const validPatientRequestWithPersonForeignIDMock: CreatePatientDto = {
+  id: 1,
+  cdPersonForeignID: '0000000000000',
+  cdPersonFirstName: 'John',
+  cdPersonLastName: 'Doe',
+  cdPersonPhone1: '0811231234',
+  emPatientCommitStatusCode: 1,
+  crProvinceCode: '00',
+  crAmpurCode: '01',
+};
+
+export const validPatientRequestWithPersonPassportIDMock: CreatePatientDto = {
+  id: 1,
+  cdPersonPassportID: '0000000000000',
+  cdPersonFirstName: 'John',
+  cdPersonLastName: 'Doe',
+  cdPersonPhone1: '0811231234',
+  emPatientCommitStatusCode: 1,
+  crProvinceCode: '00',
+  crAmpurCode: '01',
+};
+
+export const invalidPatientRequestWithoutAnyIDMock: CreatePatientDto = {
+  id: 1,
+  cdPersonFirstName: 'John',
+  cdPersonLastName: 'Doe',
+  cdPersonPhone1: '0811231234',
+  emPatientCommitStatusCode: 1,
+  crProvinceCode: '00',
+  crAmpurCode: '01',
+};
+
+export const invalidPatientRequestWithInvalidStatusCodeMock: CreatePatientDto = {
+  id: 1,
+  cdPersonPassportID: '0000000000000',
+  cdPersonFirstName: 'John',
+  cdPersonLastName: 'Doe',
+  cdPersonPhone1: '0811231234',
+  emPatientCommitStatusCode: 10,
+  crProvinceCode: '00',
+  crAmpurCode: '01',
+};
