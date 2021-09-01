d84aa9c6003aa1c9b767082ed4ffbaf6702d72eb
yort <k.kanchanapiboon@gmail.com>
2021-09-01 23:24:16 +0700

    added data validator

--- src/controllers/patient.controller.ts
+++ src/controllers/patient.controller.ts
@@ -1,12 +1,14 @@
 import { CreatePatientDto } from '@/dtos/patient.dto';
+import { dataValidationMiddleware } from '@/middlewares/validation.middleware';
 import { producePatient } from '@/services/kafka/patient.producer';
 import { Request } from 'express';
-import { Body, Controller, HttpCode, Post, Req } from 'routing-controllers';
+import { Body, Controller, HttpCode, Post, Req, UseBefore } from 'routing-controllers';
 
 @Controller()
 export class PatientController {
   @Post('/patient')
   @HttpCode(200)
+  @UseBefore(dataValidationMiddleware(CreatePatientDto))
   async create(@Body() patientData: CreatePatientDto, @Req() req: Request) {
     await producePatient(patientData, req);
     return { data: patientData, message: 'success' };
